bb4eb0fad8781a048e5c4be0a1f7d446f6724779
Usman <niyazbekovusmon@gmail.com>
2025-04-29 23:29:44 +0500

    Create index.php

---
+++ 01/index.php
@@ -0,0 +1,37 @@
+<?php
+
+#php code here
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <h1>My first PHP page</h1>
+
+    <?php
+    echo "php inside html";
+    ?>
+
+</body>
+
+</html>
+
+<?php
+
+$a = 3; #int type value
+
+$array = [1, 2, 3, 4, 5, ""]; #array type value
+
+$bool = false; #boolean type value
+
+$string = "enter string type value";
+
+
+
+# this is a coment
+
+/* This is a multi line comment
+       yet another line of comment */
